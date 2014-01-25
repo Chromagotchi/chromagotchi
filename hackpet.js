2f891f3ce9bc722063ebd35aa4deb2e3982ef902
blakethiessen <blake.w.thiessen@gmail.com>
2014-01-25 01:03:36 -0800

    Positioned sprite absolutely and made it move to the left.

--- hackpet.js
+++ hackpet.js
@@ -8,6 +8,28 @@ $(document).ready(function() {
     $(square).click(function() {
         $(square).css('background-color', 'blue');
     });
+
+
+    var xPos = 0;
+    var mainloop = function() {
+        xPos += 5;
+        $(square).css("left", xPos + "px");
+    };
+
+    var animFrame = window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame    ||
+        window.oRequestAnimationFrame      ||
+        window.msRequestAnimationFrame     ||
+        null ;
+
+    var recursiveAnim = function() {
+        mainloop();
+        animFrame( recursiveAnim );
+    };
+
+    // start the mainloop
+    animFrame( recursiveAnim );
 });
 
 //
