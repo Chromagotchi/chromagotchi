5c027f58637225513d8695a12d554429ca3aa9d8
blakethiessen <blake.w.thiessen@gmail.com>
2014-01-25 00:40:28 -0800

    Added css for content sheet (hackpet.js). Renamed style.css to popup.css. Changed hackpet.js code to jquery.

--- hackpet.js
+++ hackpet.js
@@ -1,19 +1,12 @@
 // Init code
-console.log("TEST1");
 $(document).ready(function() {
-    console.log("TEST2");
-    var square = document.createElement("div");
-    square.style.width = "100px";
-    square.style.height = "100px";
-    square.style.backgroundColor = "red";
+    var square = $(document.createElement("div"));
+    square.addClass("pet");
 
-    square.id = "square";
-
-    document.body.appendChild(square);
+    $("body").append(square);
 
     $(square).click(function() {
-        console.log("ONCLICK TEST");
-        square.style.backgroundColor = "blue";    
+        $(square).css('background-color', 'blue');
     });
 });
 
