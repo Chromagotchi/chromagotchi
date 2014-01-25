a1857c040e1102d5d9f450cf647f9749d3e8b50e
Kelvin Lee <KelvinL.email@gmail.com>
2014-01-25 03:51:02 -0800

    Forgot states.js

---
+++ content/states.js
@@ -0,0 +1,17 @@
+PetState = {
+    WANDERING : 0,
+    SLEEPING : 1,
+    TIRED : 2,
+    EATING : 3,
+    HUNGRY : 4,
+    PLAYING : 5,
+    NAUGHTY : 6
+}
+
+MovementState = {
+    LEFT : 0,
+    RIGHT : 1,
+    STOP : 2
+}
+
+
